0b72ae3da7ca47ac9904c1d89b49f7bd89ef16bf
sparrow <dirk.possardt1@freenet.de>
2020-11-11 18:48:35 +0100

    chore: changed the frequency to 16Mhz and enabled the Stellaris        Debugger. Also uncommented the while loop.

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ void BookExamples(void){ // examples from the book
   int32_t xx = 100;
   int16_t yy = -100;
   float zz = 3.14159265;
-  printf("Hello world\n");	//Hello world
+  printf("\nHello world!\n");	//Hello world
   printf("cc = %c %d %#x\n",cc,cc,cc);	//cc = V 86 0x56
   printf("xx = %c %d %#x\n",xx,xx,xx);	//xx = d 100 0x64
   printf("yy = %d %#x\n",yy,yy);	//yy = -100 0xffffff9c
@@ -42,11 +42,11 @@ int main(void){ int32_t i,n;
   BookExamples();
   n = 0;
   while(1){
-    printf("\ni="); 
-    for(i=0; i<5; i++){
-      printf("%d ",i+n);
-    }
+//    printf("\ni="); 
+//    for(i=0; i<5; i++){
+//      printf("%d ",i+n);
+//    }
 
-    n = n+1000000; // notice what happend when this goes above 2,147,483,647
+//    n = n+1000000; // notice what happend when this goes above 2,147,483,647
   }
 }
